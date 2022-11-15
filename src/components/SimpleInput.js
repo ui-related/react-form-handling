f3475ee35810611eb28f2446201bd31a7e984307
Saroj Kumar <kumarsaroj18@gmail.com>
2022-11-15 22:01:47 +0530

    Ch16 Added better validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,8 +1,12 @@
 import {useRef, useState} from "react";
 
 const SimpleInput = (props) => {
-    const [enteredName, setEnteredName] = useState('');
     const nameInputRef = useRef();
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+    const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+
     const nameInputChangeHandler = event => {
         event.preventDefault();
         setEnteredName(event.target.value);
@@ -10,21 +14,24 @@ const SimpleInput = (props) => {
 
     const formSubmissionHandler = (event) => {
         event.preventDefault();
-
+        setEnteredNameTouched(true);
         if (enteredName.trim() === '') {
+            setEnteredNameIsValid(false);
             return;
         }
+        setEnteredNameIsValid(true);
         console.log(enteredName);
         const enteredNameValue = nameInputRef.current.value;
         console.log(enteredNameValue);
 
         setEnteredName('');
     }
-
+    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+    const nameInputClasses = nameInputIsInvalid ? 'form-control invalid':'form-control';
 
     return (
         <form onSubmit={formSubmissionHandler}>
-            <div className='form-control'>
+            <div className={nameInputClasses}>
                 <label htmlFor='name'>Your Name</label>
                 <input
                     ref={nameInputRef}
@@ -32,6 +39,7 @@ const SimpleInput = (props) => {
                     id='name'
                     value={enteredName}
                     onChange={nameInputChangeHandler}/>
+                {nameInputIsInvalid && <p className='error-text'>Name must not be empty</p>}
             </div>
             <div className="form-actions">
                 <button>Submit</button>
