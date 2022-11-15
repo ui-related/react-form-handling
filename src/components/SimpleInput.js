6da0b3eb50fef48a6c56dbab5be1d4c19b998829
Saroj Kumar <kumarsaroj18@gmail.com>
2022-11-15 17:09:08 +0530

    Ch16 Added basic implmentation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -10,6 +10,10 @@ const SimpleInput = (props) => {
 
     const formSubmissionHandler = (event) => {
         event.preventDefault();
+
+        if (enteredName.trim() === '') {
+            return;
+        }
         console.log(enteredName);
         const enteredNameValue = nameInputRef.current.value;
         console.log(enteredNameValue);
